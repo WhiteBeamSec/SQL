19a4b8a63d698c1530be99223134c6d648482737
Nathan Nye <nnye@whitebeamsec.com>
2021-02-26 21:17:27 -0500

    BLAKE3 hashes of rustlang/rust:nightly for integration tests

--- sql/platforms/linux/Test.sql
+++ sql/platforms/linux/Test.sql
@@ -9,12 +9,12 @@ INSERT INTO Log (class, desc, ts) VALUES ((SELECT id FROM LogClass WHERE class="
                                          ((SELECT id FROM LogClass WHERE class="General"), "Received request for public key from 172.16.0.2", strftime("%s", "now"));
 
 -- Whitelist: Libraries will go here too
-INSERT INTO Whitelist (path, value, class) VALUES ("/bin/bash", "900c28f35811948f08f1e9b0e357b18d3bd58c8f535c0f1fd66f16838c5b6fed", (SELECT id FROM WhitelistClass WHERE class="Hash/BLAKE3")),
-                                                  ("/bin/sh", "81ead1a649fc4a85720ad69287c9c7557a787a6c590cbfffd9137b3d514164ee", (SELECT id FROM WhitelistClass WHERE class="Hash/BLAKE3")),
-                                                  ("/usr/bin/bash", "900c28f35811948f08f1e9b0e357b18d3bd58c8f535c0f1fd66f16838c5b6fed", (SELECT id FROM WhitelistClass WHERE class="Hash/BLAKE3")),
-                                                  ("/usr/bin/sh", "81ead1a649fc4a85720ad69287c9c7557a787a6c590cbfffd9137b3d514164ee", (SELECT id FROM WhitelistClass WHERE class="Hash/BLAKE3")),
+INSERT INTO Whitelist (path, value, class) VALUES ("/bin/bash", "664d9dd14597b83aebf765d2d054fa40ad7a93ffeca43ee3bba596517db2c39b", (SELECT id FROM WhitelistClass WHERE class="Hash/BLAKE3")),
+                                                  ("/bin/sh", "30751ae1ba8597ee7d9aa7e3852a58d9b21a14b88e423cbb0aa7d0512d059a6a", (SELECT id FROM WhitelistClass WHERE class="Hash/BLAKE3")),
+                                                  ("/usr/bin/bash", "664d9dd14597b83aebf765d2d054fa40ad7a93ffeca43ee3bba596517db2c39b", (SELECT id FROM WhitelistClass WHERE class="Hash/BLAKE3")),
+                                                  ("/usr/bin/sh", "30751ae1ba8597ee7d9aa7e3852a58d9b21a14b88e423cbb0aa7d0512d059a6a", (SELECT id FROM WhitelistClass WHERE class="Hash/BLAKE3")),
                                                   ("/usr/sbin/apache2", "4aadc76a6af5d65197cb9cdf7d7a6945772539c48c0120919f38f77af29c0f53", (SELECT id FROM WhitelistClass WHERE class="Hash/BLAKE3")),
-                                                  ("/usr/bin/whoami", "6d9bffe11b7c5520adb3f464a7d58c25d99ced49b881e163b7a8b09c94521897", (SELECT id FROM WhitelistClass WHERE class="Hash/BLAKE3")),
+                                                  ("/usr/bin/whoami", "5ff499d1ce89604780cd1b2d85be2a10b5076cf941950ab53a8ba092815efa7b", (SELECT id FROM WhitelistClass WHERE class="Hash/BLAKE3")),
                                                   ("/bin/bash", "/usr/sbin/apache2", (SELECT id FROM WhitelistClass WHERE class="Filesystem/Path/Executable")),
                                                   ("/bin/sh", "/usr/sbin/apache2", (SELECT id FROM WhitelistClass WHERE class="Filesystem/Path/Executable")),
                                                   ("ANY", "/usr/bin/whoami", (SELECT id FROM WhitelistClass WHERE class="Filesystem/Path/Executable")),
