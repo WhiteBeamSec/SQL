d2158facfda1037260e67c69f581214e0c1f5d20
Nathan Nye <nnye@whitebeamsec.com>
2022-06-21 11:59:31 -0400

    Formatting and position changes (cont.)

--- sql/platforms/linux/Advanced.sql
+++ sql/platforms/linux/Advanced.sql
@@ -10,34 +10,46 @@ Version: 0.3.0-dev
 -- TODO: mkfifo*?
 -- TODO 0.4.0: RedirectFunction all MemoryProtection functions to FORTIFY_SOURCE equivalents?
 
+CREATE TEMPORARY TABLE IF NOT EXISTS global_const
+AS SELECT (SELECT id FROM HookLanguage WHERE language="C") AS C,
+          (SELECT "/lib/" || (SELECT value FROM Setting WHERE param="SystemArchitecture") || "-linux-gnu/") AS LibraryPath;
+
 -- Hook
 INSERT INTO Hook (symbol, library, enabled, language, class)
-WITH const (arch) AS (SELECT value FROM Setting WHERE param="SystemArchitecture")
+WITH local_const AS (SELECT ((SELECT LibraryPath FROM global_const) || "libc.so.6") AS libc,
+                            ((SELECT LibraryPath FROM global_const) || "libpam.so.0") AS libpam,
+                            (SELECT id FROM HookClass WHERE class="Bruteforce") AS Bruteforce,
+                            (SELECT id FROM HookClass WHERE class="MemoryProtection") AS MemoryProtection)
 SELECT * FROM (VALUES -- TODO: Certificate
                       -- Bruteforce
-                      ("pam_authenticate", "/lib/" || (SELECT const.arch FROM const) || "-linux-gnu/libpam.so.0", 0, (SELECT id FROM HookLanguage WHERE language="C"), (SELECT id FROM HookClass WHERE class="Bruteforce")),
+                      ("pam_authenticate", (SELECT libpam FROM local_const), FALSE, (SELECT C FROM global_const), (SELECT Bruteforce FROM local_const)),
                       -- MemoryProtection
-                      ("gets", "/lib/" || (SELECT const.arch FROM const) || "-linux-gnu/libc.so.6", 0, (SELECT id FROM HookLanguage WHERE language="C"), (SELECT id FROM HookClass WHERE class="MemoryProtection")),
-                      ("strcat", "/lib/" || (SELECT const.arch FROM const) || "-linux-gnu/libc.so.6", 0, (SELECT id FROM HookLanguage WHERE language="C"), (SELECT id FROM HookClass WHERE class="MemoryProtection")),
-                      ("strcpy", "/lib/" || (SELECT const.arch FROM const) || "-linux-gnu/libc.so.6", 0, (SELECT id FROM HookLanguage WHERE language="C"), (SELECT id FROM HookClass WHERE class="MemoryProtection")));
+                      ("gets", (SELECT libc FROM local_const), FALSE, (SELECT C FROM global_const), (SELECT MemoryProtection FROM local_const)),
+                      ("strcat", (SELECT libc FROM local_const), FALSE, (SELECT C FROM global_const), (SELECT MemoryProtection FROM local_const)),
+                      ("strcpy", (SELECT libc FROM local_const), FALSE, (SELECT C FROM global_const), (SELECT MemoryProtection FROM local_const)));
+
+CREATE TEMPORARY VIEW LibcHook AS SELECT id, symbol FROM Hook WHERE library = (SELECT LibraryPath FROM global_const) || "libc.so.6";
+CREATE TEMPORARY VIEW LibpamHook AS SELECT id, symbol FROM Hook WHERE library = (SELECT LibraryPath FROM global_const) || "libpam.so.0";
 
 -- Argument
 INSERT INTO Argument (name, position, hook, datatype)
-WITH const (arch) AS (SELECT value FROM Setting WHERE param="SystemArchitecture")
+WITH local_const AS (SELECT (SELECT id FROM Datatype WHERE datatype="IntegerSigned") AS IntegerSigned,
+                            (SELECT id FROM Datatype WHERE datatype="String") AS String,
+                            (SELECT id FROM Datatype WHERE datatype="StructPointer") AS StructPointer)
 SELECT * FROM (VALUES -- TODO: Certificate
                       -- Bruteforce
                       -- pam_authenticate
-                      ("pamh", 0, (SELECT id FROM Hook WHERE library = "/lib/" || (SELECT const.arch FROM const) || "-linux-gnu/libpam.so.0" AND symbol="pam_authenticate"), (SELECT id FROM Datatype WHERE datatype="StructPointer")),
-                      ("flags", 1, (SELECT id FROM Hook WHERE library = "/lib/" || (SELECT const.arch FROM const) || "-linux-gnu/libpam.so.0" AND symbol="pam_authenticate"), (SELECT id FROM Datatype WHERE datatype="IntegerSigned")),
+                      ("pamh", 0, (SELECT id FROM LibpamHook WHERE symbol="pam_authenticate"), (SELECT StructPointer FROM local_const)),
+                      ("flags", 1, (SELECT id FROM LibpamHook WHERE symbol="pam_authenticate"), (SELECT IntegerSigned FROM local_const)),
                       -- MemoryProtection
                       -- gets
-                      ("s", 0, (SELECT id FROM Hook WHERE library = "/lib/" || (SELECT const.arch FROM const) || "-linux-gnu/libc.so.6" AND symbol="gets"), (SELECT id FROM Datatype WHERE datatype="String")),
+                      ("s", 0, (SELECT id FROM LibcHook WHERE symbol="gets"), (SELECT String FROM local_const)),
                       -- strcat
-                      ("dest", 0, (SELECT id FROM Hook WHERE library = "/lib/" || (SELECT const.arch FROM const) || "-linux-gnu/libc.so.6" AND symbol="strcat"), (SELECT id FROM Datatype WHERE datatype="String")),
-                      ("src", 1, (SELECT id FROM Hook WHERE library = "/lib/" || (SELECT const.arch FROM const) || "-linux-gnu/libc.so.6" AND symbol="strcat"), (SELECT id FROM Datatype WHERE datatype="String")),
+                      ("dest", 0, (SELECT id FROM LibcHook WHERE symbol="strcat"), (SELECT String FROM local_const)),
+                      ("src", 1, (SELECT id FROM LibcHook WHERE symbol="strcat"), (SELECT String FROM local_const)),
                       -- strcpy
-                      ("dest", 0, (SELECT id FROM Hook WHERE library = "/lib/" || (SELECT const.arch FROM const) || "-linux-gnu/libc.so.6" AND symbol="strcpy"), (SELECT id FROM Datatype WHERE datatype="String")),
-                      ("src", 1, (SELECT id FROM Hook WHERE library = "/lib/" || (SELECT const.arch FROM const) || "-linux-gnu/libc.so.6" AND symbol="strcpy"), (SELECT id FROM Datatype WHERE datatype="String")));
+                      ("dest", 0, (SELECT id FROM LibcHook WHERE symbol="strcpy"), (SELECT String FROM local_const)),
+                      ("src", 1, (SELECT id FROM LibcHook WHERE symbol="strcpy"), (SELECT String FROM local_const)));
 
 -- TODO: Rule
 
