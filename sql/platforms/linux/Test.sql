0dfe27f0219f677b527d4f5dc5e53b70e1893ead
Nathan Nye <nnye@whitebeamsec.com>
2022-07-02 13:46:08 -0400

    Add Test HookClass

--- sql/platforms/linux/Test.sql
+++ sql/platforms/linux/Test.sql
@@ -10,6 +10,9 @@ Version: 0.3.0-dev
 CREATE TEMPORARY TABLE IF NOT EXISTS global_const
 AS SELECT (SELECT "/lib/" || (SELECT value FROM Setting WHERE param="SystemArchitecture") || "-linux-gnu/") AS LibraryPath;
 
+-- HookClass
+INSERT OR IGNORE INTO HookClass (class) VALUES ("Test");
+
 -- Whitelist
 INSERT OR IGNORE INTO Whitelist (parent, path, value, class)
 WITH local_const AS (SELECT (SELECT id FROM WhitelistClass WHERE class="Filesystem/Path/Executable") AS Executable,
